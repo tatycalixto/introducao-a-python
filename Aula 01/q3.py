b306f4488946a66f5e3e1fdb9865bec06759e016
Taty Calixto <54193277+tatycalixto@users.noreply.github.com>
2024-03-13 20:40:40 -0300

    Create q3.py

---
+++ Aula 01/q3.py
@@ -0,0 +1,4 @@
+qtd_horas = int(input("Digite o valor de cada hora trabalhada: "))
+horas_mes = int(input("Digite o número de horas trabalhadas por mês: "))
+salario = qtd_horas*horas_mes
+print(" Seu salário é: ",salario)
